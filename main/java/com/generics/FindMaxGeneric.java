cbba80c03c21f87de3ef5fff16887a29c3765a6f
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-16 03:52:19 +0530

    Added: Welcome to Generics Problem

---
+++ main/java/com/generics/FindMaxGeneric.java
@@ -0,0 +1,15 @@
+//compareTo -- boolean
+//https://deepeshdarshan.wordpress.com/2013/01/17/finding-max-value-using-java-generics/
+package com.generics;
+
+public class FindMaxGeneric{
+    public <T extends Comparable>T findMaximum(T[] numberArray) {
+        T max = numberArray[0];
+        for (T element : numberArray) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+        return max;
+    }
+}
