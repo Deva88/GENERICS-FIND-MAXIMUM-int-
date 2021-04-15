77439415fab7981ed9818579a40b5b91ed2ba242
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-16 04:34:08 +0530

    Given 3 String Find the Maximum

--- main/java/com/generics/FindMaxGeneric.java
+++ main/java/com/generics/FindMaxGeneric.java
@@ -1,7 +1,7 @@
 //compareTo -- boolean
 //https://deepeshdarshan.wordpress.com/2013/01/17/finding-max-value-using-java-generics/
 package com.generics;
-/*@Description:- Given 3 Floats find the maximum
+/*@Description:- Given 3 Strings find the maximum
  *Given Max at 1st Position return the Same Number
  *Given Max at 2nd Position return the Same Number
  *Given Max at 3rd Position return the Same Number
