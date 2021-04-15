0f17ea5d6332ebb951e5bc1d977cd2ded765bbcf
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-16 03:58:06 +0530

    Added: Given 3 Integers Find the maximum

--- main/java/com/generics/FindMaxGeneric.java
+++ main/java/com/generics/FindMaxGeneric.java
@@ -1,6 +1,11 @@
 //compareTo -- boolean
 //https://deepeshdarshan.wordpress.com/2013/01/17/finding-max-value-using-java-generics/
 package com.generics;
+/*@Description:- Given 3 Integers find the maximum
+*Given Max Number at 1st Position return the Same Number
+* Given Max Number at 2nd Position return the Same Number
+* Given Max Number at 3rd Position return the Same Number
+ */
 
 public class FindMaxGeneric{
     public <T extends Comparable>T findMaximum(T[] numberArray) {
