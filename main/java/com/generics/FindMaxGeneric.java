b35b1437224fbdb517209bb167a5247aaf06977c
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-16 04:55:35 +0530

    added: Ectending the program

--- main/java/com/generics/FindMaxGeneric.java
+++ main/java/com/generics/FindMaxGeneric.java
@@ -1,31 +1,40 @@
 package com.generics;
-/*@Description:- Refactor all the 3 to One Generic Method and find the maximum
- *Refactor to create Generic Class to take in 3 variables of Generic Type
+/*
+ *@Description:- Extend the max method to take more then three parameters
  */
 public class FindMaxGeneric<T extends Comparable<T>> {
+    private final T a ;T b;T c;T p;T q;T r;
 
-    private final T a;
-    private final T b;
-    private final T c;
-
-    public FindMaxGeneric(T a, T b, T c) {
+    public FindMaxGeneric(T a, T b, T c,T p,T q,T r) {
         this.a =a;
         this.b = b;
         this.c = c;
+        this.p =p;
+        this.q = q;
+        this.r = r;
     }
 
     public T findMaximum() {
-        return FindMaxGeneric.findMaximum(a, b, c);
+        return FindMaxGeneric.findMaximum(a, b, c,p ,q ,r);
     }
 
     //Single generic method to compare different types of data
-    public static <T extends Comparable> T findMaximum(T a, T b, T c) {
+    public static <T extends Comparable> T findMaximum(T a, T b, T c,T p,T q,T r) {
         T max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
         if (c.compareTo(max) > 0) {
             max = c;
+        }
+        if (p.compareTo(max) > 0) {
+            max = p;
+        }
+        if (q.compareTo(max) > 0) {
+            max = q;
+        }
+        if (r.compareTo(max) > 0) {
+            max = r;
         }
         return max;
     }
