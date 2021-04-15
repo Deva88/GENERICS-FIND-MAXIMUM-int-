63fe932be79a6002d633ed32d148cfdab7e10a01
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-16 05:01:31 +0530

    added: maximum method and generic class

--- main/java/com/generics/FindMaxGeneric.java
+++ main/java/com/generics/FindMaxGeneric.java
@@ -1,10 +1,9 @@
 package com.generics;
 /*
- *@Description:- Extend the max method to take more then three parameters
+ *@Description:- Extend the max method to also print the max to std out using Generic Method
  */
 public class FindMaxGeneric<T extends Comparable<T>> {
     private final T a ;T b;T c;T p;T q;T r;
-
     public FindMaxGeneric(T a, T b, T c,T p,T q,T r) {
         this.a =a;
         this.b = b;
@@ -12,13 +11,20 @@ public class FindMaxGeneric<T extends Comparable<T>> {
         this.p =p;
         this.q = q;
         this.r = r;
-    }
 
+    }
     public T findMaximum() {
         return FindMaxGeneric.findMaximum(a, b, c,p ,q ,r);
     }
 
-    //Single generic method to compare different types of data
+    public T findmaximum() {
+        T findMax = FindMaxGeneric.findMaximum(a, b, c, p, q,r);
+        printMax(findMax);
+        return findMax;
+    }
+    public void printMax(T maximum) {
+        System.out.println("Maximum : " + maximum);
+    }
     public static <T extends Comparable> T findMaximum(T a, T b, T c,T p,T q,T r) {
         T max = a;
         if (b.compareTo(max) > 0) {
